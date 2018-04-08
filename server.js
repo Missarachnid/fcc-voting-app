8c7a691f86e9946420909bc5a1d4392187811266
Glitch (voting-app-mk) <none>
2018-04-08 01:48:50 +0000

    💋🐯 Checkpoint ./server.js:503759/1032 ./views/index.html:503759/441 ./.glitch-assets:503759/390

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-// server.js
-// where your node app starts
+/*
+1. As an authenticated user, I can keep my polls and come back later to access them.
+2. As an authenticated user, I can share my polls with my friends.
+3. As an authenticated user, I can see the aggregate results of my polls.
+4. User Story: As an authenticated user, I can delete polls that I decide I don't want anymore.
+5. As an authenticated user, I can create a poll with any number of possible items.
+6. As an unauthenticated or authenticated user, I can see and vote on everyone's polls.
+7. As an unauthenticated or authenticated user, I can see the results of polls in chart form. (This could be implemented using Chart.js or Google Charts.)
+8. As an authenticated user, if I don't like the options on a poll, I can create a new option.
+*/
 
-// init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
@@ -16,7 +19,9 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
+
+
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
